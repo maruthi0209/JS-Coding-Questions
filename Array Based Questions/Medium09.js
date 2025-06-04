05ab2f55b7f99ec245429057bdcb1d740d5014f7
maruthi0209 <sethumaruthi93@gmail.com>
2025-06-04 17:11:18 +0530

    modified medium09 to remove comments

--- Array Based Questions/Medium09.js
+++ Array Based Questions/Medium09.js
@@ -5,13 +5,6 @@ Write a function to flatten a nested array into a single array.
  */
 
 function flattenNested(arr) {
-    // let temp=[];
-    // for(element of arr) {
-    //     if (Array.isArray(element)){
-    //         element = element.flat()
-    //     }
-    //     temp.push(element)
-    // }
     while(!arr.every(element => typeof element === 'number')) {
         arr = arr.flat()
     }
